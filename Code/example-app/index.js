ee476cb276b35da55c3c97277efe2a8e6ff552e0
Joey Lawrance <lawrancej@wit.edu>
2016-01-19 11:52:38 -0500

    Moar stuff about handlebars.

--- Code/example-app/index.js
+++ Code/example-app/index.js
@@ -1,14 +1,32 @@
 // this is essentially an import
 var express = require('express');
+// import express handlebars
+var exphbs  = require('express-handlebars');
 // this lazily loads the module
 var app = express();
+
+// tell express (the web framework) to use handebars
+// and we're going to use main as the default layout
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// this speaks for itself, right?
+app.set('view engine', 'handlebars');
 
 // serve up any request to /static using files from public
 app.use('/static', express.static('public'));
 
+var viewCounter = 1000;
 // Respond to a request
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('home', {
+    "title": "Something",
+    "views": viewCounter,
+    "list": [
+      {"title": "something", "body": "moar stuff"},
+      {"title": "another thing", "body": "even more!"}
+    ]
+  });
+  viewCounter++;
+//  res.send('Hello World!');
 });
 
 // Listen as a server
